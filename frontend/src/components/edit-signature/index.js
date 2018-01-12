4bb2f0aa3dcea3ec02fd236bddb5132f94f0aa8d
Francois Constant <francois.constant@gmail.com>
2018-01-12 20:47:47 +0900

    Add link to /u/user-slug/1234567890/update-signature/

---
+++ frontend/src/components/edit-signature/index.js
@@ -0,0 +1,16 @@
+/* jshint ignore:start */
+import React from 'react';
+
+
+export default class extends React.Component {
+  /* THIS IS NOT USED AT THE MOMENT - see iwcforum/iwcforum/urls.py */
+  render() {
+    return (
+      <div className="panel panel-default panel-form">
+        <div className="panel-heading">
+          <h3 className="panel-title">Signature</h3>
+        </div>
+      </div>
+    );
+  }
+}
